c1adee9b8c834fb6e19c0a83c681a2fa6c22f007
João Victor <joao.lenny100@gmail.com>
2022-11-25 15:16:48 -0300

    React
    - Removendo mais alguns arquivos;
    - Editando index.js.

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import App from './App';
 import './css/index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
