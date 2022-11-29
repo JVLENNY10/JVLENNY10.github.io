f8ba1933ff28246828cd9c81dc9e6d590cb3866d
João Victor <joao.lenny100@gmail.com>
2022-11-29 08:49:22 -0300

    Pages - Home
    - Adicionando article com boas vindas.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
+import { Link } from 'react-router-dom';
+
+import Header from '../components/Header';
+
 function Home() {
   return (
     <>
-      <h1>Olá, mundo</h1>
+      <Header />
+
+      <main>
+        <article>
+          <h1>Seja bem vindo!</h1>
+
+          <p>
+            Aqui você vai encontrar informações sobre mim,
+            minhas habilidades e meus projetos.
+          </p>
+
+          <p>
+            <Link to="/contact">
+              Entre em contato clicando aqui
+            </Link>
+          </p>
+        </article>
+      </main>
     </>
   );
 }
