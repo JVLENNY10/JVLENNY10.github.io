56dd0057a929b3dacf510e363113b33c4ad40431
João Victor <joao.lenny100@gmail.com>
2022-12-04 12:31:54 -0300

    Routes.jsx
    - Criando rota para a page "Bio.jsx".

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,11 +1,14 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Bio from './pages/Bio';
 import Home from './pages/Home';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
         <Route component={ Home } exact path='/' />
+        <Route component={ Bio } exact path='/bio' />
       </Switch>
     </BrowserRouter >
   );
