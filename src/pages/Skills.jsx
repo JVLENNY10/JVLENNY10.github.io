3699e854a5f169d674996aeabe58b66af5ddab13
João Victor <joao.lenny100@gmail.com>
2022-12-04 13:20:21 -0300

    Pages - Skills.jsx
    - Criando arquivo "Skills.jsx";
    - Importando componente "Header";
    - Implementando função "Skills";
    - Criando "section";
    - Adicionando className "skills-section".

---
+++ src/pages/Skills.jsx
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+
+function Skills() {
+  return (
+    <>
+      <Header />
+
+      <section className="skills-section" />
+    </>
+  );
+}
+
+export default Skills;
