d856887926b5ae84a4633a55dd832285639cbb5d
João Victor <joao.lenny100@gmail.com>
2022-12-04 13:29:17 -0300

    Pages - Skills.jsx
    - Adicionando 2 tags "article";
    - Adicionando skills de Front-End;
    - Adicionando skills de Back-End.

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -5,7 +5,29 @@ function Skills() {
     <>
       <Header />
 
-      <section className="skills-section" />
+      <section className="skills-section">
+        <article>
+          <h2>Front-End</h2>
+          <ul>
+            <li>HTML5;</li>
+            <li>CSS3;</li>
+            <li>JavaScript ES6+ / TypeScript;</li>
+            <li>React Js;</li>
+            <li>Redux / Context API.</li>
+          </ul>
+        </article>
+
+        <article>
+          <h2>Back-End</h2>
+          <ul>
+            <li>Docker;</li>
+            <li>Node Js / TypeScript;</li>
+            <li>Sequelize;</li>
+            <li>MySQL / MongoDB / PostgreSQL;</li>
+            <li>Python.</li>
+          </ul>
+        </article>
+      </section>
     </>
   );
 }
