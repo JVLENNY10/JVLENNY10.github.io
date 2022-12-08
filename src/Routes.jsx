6f5092b5fd5e1838b0dccaae0a2922e807629fee
João Victor <joao.lenny100@gmail.com>
2022-12-08 20:41:01 -0300

    Routes.jsx
    - Criando rota "/projects".

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Bio from './pages/Bio';
 import Home from './pages/Home';
 import Skills from './pages/Skills';
+import Projects from './pages/Projects';
 
 function Routes() {
   return (
@@ -11,6 +12,7 @@ function Routes() {
         <Route component={ Home } exact path='/' />
         <Route component={ Bio } exact path='/bio' />
         <Route component={ Skills } exact path='/skills' />
+        <Route component={ Projects } exact path='/projects' />
       </Switch>
     </BrowserRouter >
   );
