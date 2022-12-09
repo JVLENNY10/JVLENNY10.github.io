7ffbce614db15164420b06dbef3411798be95ade
João Victor <joao.lenny100@gmail.com>
2022-12-09 11:09:46 -0300

    pages - Projects.jsx
    - Importando arquivo ".css";
    - Removendo algumas divs;
    - Englobando conteúdo com uma section;
    - Adicionando classes;
    - Criando caixa para projetos front-end;
    - Criando lista para projetos front-end;
    - Criando caixa para projetos back-end;
    - Criando lista para projetos back-end.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 
+import '../css/pages/projects.css';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 
@@ -8,27 +9,79 @@ function Projects() {
     <>
       <Header />
 
-      <div className="projects-page">
+      <section className="projects-section">
+        <div className="projects">
+          <h1>Front-End</h1>
 
-        <div className="projects-section">
-          <div>
-            <ul>
-              <li>
-                <article>
-                  <h2>Trybetunes</h2>
-                  <nav>
-                    <Link to="/trybetunes">Projeto</Link>
-                    <a href="https://github.com/JVLENNY10/trybetunes">
-                      Repositório do projeto
-                    </a>
-                  </nav>
-                </article>
-              </li>
-            </ul>
-          </div>
+          <ul>
+            <li>
+              <div>
+                <h2>Solar System</h2>
+                <Link to="/solar-system">Projeto</Link>
+                <a href="https://github.com/JVLENNY10/solar-system">
+                  Repositório do projeto
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div>
+                <h2>Trybetunes</h2>
+                <Link to="/trybetunes">Projeto</Link>
+                <a href="https://github.com/JVLENNY10/trybetunes">
+                  Repositório do projeto
+                </a>
+              </div>
+            </li>
+          </ul>
         </div>
 
-      </div>
+        <div className="projects">
+          <h1>Back-End</h1>
+
+          <ul>
+            <li>
+              <div>
+                <h2>Blogs API</h2>
+                <Link to="*">Apenas Back-End</Link>
+                <a href="https://github.com/JVLENNY10/blogs-api">
+                  Repositório do projeto
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div>
+                <h2>Talker Manager</h2>
+                <Link to="*">Apenas Back-End</Link>
+                <a href="https://github.com/JVLENNY10/talker-manager">
+                  Repositório do projeto
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div>
+                <h2>Trybe Futebol Clube</h2>
+                <Link to="*">Apenas Back-End</Link>
+                <a href="https://github.com/JVLENNY10/trybe-futebol-clube">
+                  Repositório do projeto
+                </a>
+              </div>
+            </li>
+
+            <li>
+              <div>
+                <h2>Trybesmith</h2>
+                <Link to="*">Apenas Back-End</Link>
+                <a href="https://github.com/JVLENNY10/trybesmith">
+                  Repositório do projeto
+                </a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </section>
 
       <Footer />
     </>
