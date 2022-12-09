a75d68fa3305f3aabf68b28eb8b0f82e30f70f4f
João Victor <joao.lenny100@gmail.com>
2022-12-09 14:23:42 -0300

    Components - Footer.jsx
    - Editando algumas tags;
    - Editando algumas classes.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,38 +6,35 @@ import instagram from '../images/instagram.png';
 function Footer() {
   return (
     <footer>
-      <div className="div-footer">
-        <nav className="social-networks">
-          <a
-            href="https://github.com/JVLENNY10"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <img alt="Logo do GitHub" src={ github } />
-          </a>
+      <nav className="social-networks">
+        <a
+          href="https://github.com/JVLENNY10"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img alt="Logo do GitHub" src={ github } />
+        </a>
 
-          <a
-            className="instagram-link"
-            href="https://www.instagram.com/jv_lenny10/"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <img alt="Logo do Instagram" src={ instagram } />
-          </a>
+        <a
+          href="https://www.instagram.com/jv_lenny10/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img alt="Logo do Instagram" src={ instagram } />
+        </a>
 
-          <a
-            href="https://www.linkedin.com/in/jvlenny10/"
-            rel="noreferrer"
-            target="_blank"
-          >
-            <img alt="Logo do LinkedIn" src={ linkedin } />
-          </a>
-        </nav>
+        <a
+          href="https://www.linkedin.com/in/jvlenny10/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img alt="Logo do LinkedIn" src={ linkedin } />
+        </a>
+      </nav>
 
-        <div className="contact">
-          <p>E-Mail: joao.lenny100@gmail.com</p>
-          <p className="cell">Celular: (21)97293-7681</p>
-        </div>
+      <div className="contact">
+        <p>E-Mail: joao.lenny100@gmail.com</p>
+        <p className="cell">Celular: (21)97293-7681</p>
       </div>
     </footer>
   );
