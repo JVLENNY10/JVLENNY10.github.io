6815f00314f33f9053c83b0dc4fca3487cc4e7c7
João Victor <joao.lenny100@gmail.com>
2022-12-09 13:34:26 -0300

    Pages - Skills.jsx
    - Editando algumas tags;
    - Editando algumas classes.

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -7,31 +7,29 @@ function Skills() {
     <>
       <Header />
 
-      <div className="skills-page">
-        <section className="skills-section">
-          <article className="skills-article-front">
-            <h2>Front-End</h2>
-            <ul>
-              <li>HTML5;</li>
-              <li>CSS3;</li>
-              <li>JavaScript ES6+ / TypeScript;</li>
-              <li>React Js;</li>
-              <li>Redux / Context API.</li>
-            </ul>
-          </article>
+      <section className="skills-section">
+        <div className="front-end-skills skills-container">
+          <h2>Front-End</h2>
+          <ul>
+            <li>HTML5;</li>
+            <li>CSS3;</li>
+            <li>JavaScript ES6+ / TypeScript;</li>
+            <li>React Js;</li>
+            <li>Redux / Context API.</li>
+          </ul>
+        </div>
 
-          <article className="skills-article-back">
-            <h2>Back-End</h2>
-            <ul>
-              <li>Docker;</li>
-              <li>Node Js / TypeScript;</li>
-              <li>Sequelize;</li>
-              <li>MySQL / MongoDB / PostgreSQL;</li>
-              <li>Python.</li>
-            </ul>
-          </article>
-        </section>
-      </div>
+        <div className="back-end-skills skills-container">
+          <h2>Back-End</h2>
+          <ul>
+            <li>Docker;</li>
+            <li>Node Js / TypeScript;</li>
+            <li>Sequelize;</li>
+            <li>MySQL / MongoDB / PostgreSQL;</li>
+            <li>Python.</li>
+          </ul>
+        </div>
+      </section>
 
       <Footer />
     </>
